b0c88f90decdb7ace1c0770422a1e5588e48af2d
Bill Tanthowi Jauhari <bill.tanthowi@majoo.id>
2020-11-30 21:50:01 +0700

    add description

--- verify_signature.php
+++ verify_signature.php
@@ -6,11 +6,13 @@ $data = "billtanthowijauhari";
 $pkeyid = openssl_get_privatekey("file://private.pem");
 $pubkey = openssl_get_publickey("file://public.pub");
 
-// compute signature
+// create signature
 openssl_sign($data, $signature, $pkeyid, OPENSSL_ALGO_SHA256);
 
+// print signature in base64 mode
 echo "Signature : " . base64_encode($signature) . "\n";
 
+// verify signature
 $isValid = openssl_verify($data, $signature, $pubkey, OPENSSL_ALGO_SHA256) . "\n";
 
 echo "\n Is Signature Valid ? " . (($isValid) ? "YES" : "NO")  . "\n";
