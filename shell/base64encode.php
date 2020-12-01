d18826303d4b0cf727286525ead3c4d64a0d780c
Bill Tanthowi Jauhari <bill.tanthowi@majoo.id>
2020-12-01 11:35:41 +0700

    add shell command

---
+++ shell/base64encode.php
@@ -0,0 +1,5 @@
+<?php
+
+$secretkey = "0f30d740d4c449b88967fc6569f20f4d:52df3e101c744de0bc4865bbacc8e9b8";
+
+echo base64_encode($secretkey) . "\n";
---
+++ shell/tokped_credential.sh
@@ -0,0 +1,5 @@
+curl -X POST \
+'https://accounts.tokopedia.com/token?grant_type=client_credentials' \
+-H 'Authorization: Basic MGYzMGQ3NDBkNGM0NDliODg5NjdmYzY1NjlmMjBmNGQ6NTJkZjNlMTAxYzc0NGRlMGJjNDg2NWJiYWNjOGU5Yjg=' \
+-H 'Content-Length: 0' \
+-H 'User-Agent: PostmanRuntime/7.17.1'
