4aadd2ffe427e9522bcfe579f10a31e8ea3f5829
Bill Tanthowi Jauhari <bill.tanthowi@majoo.id>
2020-12-01 16:59:59 +0700

    add tokped create, check uploadid

--- shell/tokped_check_status.sh
+++ shell/tokped_check_status.sh
@@ -1,3 +1,3 @@
 curl -X GET \
-  'https://fs.tokopedia.net/v2/products/fs/13389/status/4384905?shop_id=8212471' \
+  'https://fs.tokopedia.net/v2/products/fs/13389/status/4391144?shop_id=8212471' \
   -H 'Authorization: Bearer c:ojPG1m5JSxSLXc7l1pT6oQ'
